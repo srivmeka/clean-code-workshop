24566df26737285ff067b8046c0c1a31e99b97a7
srivathsalac <srivathsalac@10.136.20.220>
2019-09-13 14:36:58 +0530

    changes

--- src/main/java/com/movierental/Rental.java
+++ src/main/java/com/movierental/Rental.java
@@ -2,19 +2,50 @@ package com.movierental;
 
 public class Rental {
 
-  private int daysRented;
-  private Movie movie;
+	private int daysRented;
+	private Movie movie;
 
-  public Rental(Movie movie, int daysRented){
-    this.movie = movie;
-    this.daysRented = daysRented;
-  }
+	public Rental(Movie movie, int daysRented) {
+		this.movie = movie;
+		this.daysRented = daysRented;
+	}
 
-  public int getDaysRented() {
-    return daysRented;
-  }
+	public int getDaysRented() {
+		return daysRented;
+	}
 
-  public Movie getMovie() {
-    return movie;
-  }
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public double getCalculatedAmount() {
+		// determine amounts for each line
+		double amount = 0.0;
+		switch (this.getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			amount += 2;
+			if (this.daysRented > 2) {
+				amount += (this.daysRented - 2) * 1.5;
+			}
+			break;
+		case Movie.NEW_RELEASE:
+			amount += this.daysRented * 3;
+			break;
+		case Movie.CHILDRENS:
+			amount += 1.5;
+			if (this.daysRented > 3) {
+				amount += (this.daysRented - 3) * 1.5;
+			}
+			break;
+		}
+
+		return amount;
+	}
+
+	public int getCalculatedBonusRentals() {
+		if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) && this.daysRented > 1) {
+			return 1;
+		}
+		return 0;
+	}
 }
--- src/test/java/com/movierental/CustomerTest.java
+++ src/test/java/com/movierental/CustomerTest.java
@@ -1,9 +1,33 @@
 package com.movierental;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 
 public class CustomerTest {
     @Test
-    public void test(){}
+    public void testEmptyStatement(){
+    	Customer customer = new Customer("TestEmptyRentals");
+    	String result = customer.statement();
+    	System.out.println("Result: " + result);
+    	assertNotNull(result);
+    }
+   
+    
+    @Test
+    //Renter Points should be incremented for valid Movie types?
+    public void testStatement(){
+    	Customer customer = new Customer("TestRentals");
+    	Movie movie = new Movie("Movie One", 2);
+    	Rental rental1 = new Rental(movie, 2);
+    	customer.addRental(rental1);
+    	String result = customer.statement();
+    	System.out.println("Result: " + result);
+    	assertNotNull(result);
+    }
+    
+    
+    
+    
 
-}
+}  
